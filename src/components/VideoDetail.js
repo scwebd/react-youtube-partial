2d9ae4bf3e12111d1c6ba4d32c74d6907e00dfb4
scwebd <scwebd@gmail.com>
2019-02-05 21:56:54 -0700

    Partial app code from Review Session Part 1

---
+++ src/components/VideoDetail.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const VideoDetail = ({ video }) => {
+    
+    if (!video) {
+        return <p>Loading spinner goes here...</p>
+    }
+
+    const videoId = video.id.videoId;
+    const videoUrl = `https://www.youtube.com/embed/${videoId}?rel=0`;
+
+    return (
+        <>
+            <div className="embed-responsive embed-responsive-16by9">
+                <iframe className="embed-responsive-item" src={videoUrl} allowFullScreen></iframe>
+            </div>
+            <h2>{video.snippet.title}</h2>
+            <p>{video.snippet.description}</p>
+        </>
+    )
+}
+
+export default VideoDetail;
